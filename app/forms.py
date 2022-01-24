958008e4f8e0a5cf64a1f22e786228b583c208fa
Pleahy1233 <patrick.james1212@gmail.com>
2022-01-23 22:20:40 -0800

    nothing

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TelField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 
 
 class RegisterForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired()])
-  phone_number = TelField('Phone Numbers', validators=[DataRequired()])
+  phone_number = StringField('Phone Numbers', validators=[DataRequired()])
   address = StringField('Address', validators=[DataRequired()])
-  register = SubmitField('Register', validators=[DataRequired()])
+  register = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from ctypes import addressof
 from app import app
-from flask import render_template
+from flask import redirect, render_template, url_for
 from app.forms import RegisterForm
 
 
@@ -13,4 +14,12 @@ def register():
   form = RegisterForm()
   if form.validate_on_submit():
     print('FORM HAS BEEN VALIDATED')
+    name = form.name.data
+    phone_number = form.phone_number.data
+    address = form.address.data
+    register = form.register.data
+    print(name, phone_number, address, register)
+    return redirect(url_for('index'))
+
+
   return render_template('register.html', form=form)
